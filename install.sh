23dc2bc2f76b0193215a95f3875b6b8ab6493eec
Greg Tatum <tatum.creative@gmail.com>
2026-06-27 06:52:04 -0500

    Update my scripts

---
+++ install.sh
@@ -0,0 +1,13 @@
+# Install all of the things
+brew install \
+  awscli \
+  codex \
+  coreutils \
+  diff-so-fancy \
+  ffmpeg \
+  go-task \
+  nodejs \
+  tldr \
+  zsh-autosuggestions \
+  delta \
+  # Done
